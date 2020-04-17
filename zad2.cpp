c95e9d313cdf0d0e4e7c7e3b6a97a24470be2dce
Stefan Shklifov <s.shklifov@gmail.com>
2020-04-17 14:29:03 +0300

    Added new task.
    It prints inside the threads, which is wrong.

---
+++ zad2.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <pthread.h>
+#include <cassert>
+
+bool IsPrime(int d)
+{
+    assert(d >= 2);
+    for (int i = 2; i < d; ++i) {
+        if (d % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void* Thread(void* data)
+{
+    size_t n = (size_t)data;
+    size_t cnt = 0;
+    for (size_t i = 2; i <= n; ++i) {
+        if (IsPrime(i)) ++cnt;
+    }
+    printf("# primes less/eq than %lu are: %lu\n", n, cnt);
+    return NULL;
+}
+
+int main()
+{
+    // Program must read 4 integers from stdin. For each integer, it must find
+    // the number of prime numbers less than or equal to it.
+
+    int n[4];
+    scanf("%d %d %d %d", n, n + 1, n + 2, n + 3);
+
+    pthread_t threads[4];
+    for (int i = 0; i < 4; ++i) {
+        size_t threadArg = n[i];
+        int s = pthread_create(&threads[i], NULL, Thread, (void*)threadArg);
+        assert(s == 0);
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        int s = pthread_join(threads[i], NULL);
+        assert(s == 0);
+    }
+}
+
