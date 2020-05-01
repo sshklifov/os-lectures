fb4437148dc7d010e932a02783c837f5c48365ce
Stefan Shklifov <s.shklifov@gmail.com>
2020-05-01 16:01:13 +0300

    code from second lecture

--- lect2/zad1.c
+++ lect2/zad1.c
@@ -1,59 +1,60 @@
 #include <stdio.h>
-#include <assert.h>
 #include <pthread.h>
+#include <assert.h>
 
 typedef struct ThreadInfo {
     int from;
     int to;
+    long long result;
 } ThreadInfo;
 
-void* Thread(void* arg)
+void* Thread(void* p)
 {
-    ThreadInfo* pinfo = (ThreadInfo*)arg;
-    int from = pinfo->from;
-    int to = pinfo->to;
+    ThreadInfo* info = (ThreadInfo*)p;
+    int from = info->from;
+    int to = info->to;
 
     long long res = 0;
-    for (int i = from; i < to; ++i) {
-        res += i;
+    for (int noti = from; noti < to; noti++) {
+        res += noti;
     }
-    return (void*)res;
+
+    info->result = res;
+    return NULL;
 }
 
 int main()
 {
-    const int NTHREADS = 4;
     int n = 1000000000;
-
-    /// NOTE: use long long, overflow otherwise
+    const int NTHREADS = 6;
     ThreadInfo info[NTHREADS];
     pthread_t threads[NTHREADS];
 
-    int threadFrom = 0, threadTo = 0;
-    for (int i = 0; i < NTHREADS; ++i) {
-        threadFrom = threadTo;
-        if (i != NTHREADS - 1) {
-            threadTo = threadFrom + n / 4;
+    int startRange = 1;
+    int endRange;
+    for (int threadIndex = 0; threadIndex < NTHREADS; ++threadIndex) {
+        if (threadIndex != NTHREADS - 1) {
+            endRange = startRange + n / NTHREADS;
         }
         else {
-            threadTo = n + 1;
+            endRange = n + 1;
         }
-
-        info[i].from = threadFrom;
-        info[i].to = threadTo;
-        int s = pthread_create(&threads[i], NULL, Thread, &info[i]);
+        info[threadIndex].from = startRange;
+        info[threadIndex].to = endRange;
+        int s = pthread_create(&threads[threadIndex], NULL, Thread,
+                               &info[threadIndex]);
         assert(s == 0);
+        startRange = endRange;
     }
 
     long long res = 0;
-    for (int i = 0; i < NTHREADS; ++i) {
-        void* rval;
-        int s = pthread_join(threads[i], &rval);
+    for (int threadIndex = 0; threadIndex < NTHREADS; ++threadIndex) {
+        int s = pthread_join(threads[threadIndex], NULL);
         assert(s == 0);
-
-        res += (long long)rval;
+        res += info[threadIndex].result;
     }
-    printf("sum 1..%d is: %lld\n", n, res);
+
+    printf("res: %lld\n", res);
 
     return 0;
 }
--- lect2/zad2.c
+++ lect2/zad2.c
@@ -1,107 +1,109 @@
-#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
+#include <stdbool.h>
 #include <assert.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 typedef struct ThreadInfo {
-    int* start;
+    int* arr;
     int n;
     int what;
-    bool* pGlobalRes;
+    bool result;
 } ThreadInfo;
 
-void* PerThreadWork(void* arg)
+void* FindNumber(void* p)
 {
-    ThreadInfo* pInfo = (ThreadInfo*)arg;
-    int* start = pInfo->start;
-    int n = pInfo->n;
-    int what = pInfo->what;
-    bool* pGlobalRes = pInfo->pGlobalRes;
+    ThreadInfo* info = (ThreadInfo*)p;
+    int* arr = info->arr;
+    int n = info->n;
+    int what = info->what;
 
     for (int i = 0; i < n; ++i) {
-        if (*pGlobalRes) {
-            break;
-        }
-        if (start[i] == what) {
-            *pGlobalRes = 1;
-            break;
+        if (arr[i] == what) {
+            info->result = true;
+            return NULL;
         }
     }
+
+    info->result = false;
     return NULL;
 }
 
-bool DoWork(int* arr, int n, int what)
+bool TimedWork(int* arr, int n)
 {
+    int what = 987654321;
     const int NTHREADS = 4;
+    ThreadInfo infos[NTHREADS];
     pthread_t threads[NTHREADS];
-    ThreadInfo info[NTHREADS];
-    bool res = 0;
 
-    int numFinished = 0;
+    int* startAddress = arr;
+    int remainingElems = n;
     for (int i = 0; i < NTHREADS; ++i) {
-        int toFinish;
+        int toProcess;
         if (i != NTHREADS - 1) {
-            toFinish = n / NTHREADS;
+            toProcess = n / NTHREADS;
         }
         else {
-            toFinish = n - numFinished;
+            toProcess = remainingElems;
         }
-        info[i].start = arr + numFinished;
-        info[i].n = toFinish;
-        info[i].what = what;
-        info[i].pGlobalRes = &res;
-        int s = pthread_create(&threads[i], NULL, PerThreadWork, &info[i]);
+        infos[i].arr = startAddress;
+        infos[i].n = toProcess;
+        infos[i].what = what;
+
+        int s = pthread_create(&threads[i], NULL, FindNumber, &infos[i]);
         assert(s == 0);
-        numFinished += toFinish;
+        startAddress = startAddress + toProcess;
+        remainingElems -= toProcess;
     }
-    bool found = false;
-    numFinished = 0;
     for (int i = 0; i < NTHREADS; ++i) {
         int s = pthread_join(threads[i], NULL);
         assert(s == 0);
+        if (infos[i].result) {
+            return true;
+        }
     }
-    return res;
+    assert(remainingElems == 0);
+    return false;
 }
 
 int main()
 {
-    int n = 1000000000;
+    int n = 500000000;
 
-    size_t bytes = n * sizeof(int); // 4GB!
+    size_t bytes = n * sizeof(int); // 2GB!
     int* arr = (int*)malloc(bytes);
     if (arr == NULL) {
-        printf("cannot allocate 4GB\n");
+        printf("cannot allocate 2GB\n");
         return 1;
     }
     for (int i = 0; i < n; ++i) {
         arr[i] = i;
     }
 
-    struct timeval then;
-    int s = gettimeofday(&then, NULL);
-    assert(s == 0);
+    /* struct timeval then; */
+    /* int s = gettimeofday(&then, NULL); */
+    /* assert(s == 0); */
 
     printf("Starting search!\n");
-    int what = 4;
-    bool found = DoWork(arr, n, what);
+    bool found = TimedWork(arr, n);
 
-    struct timeval now;
-    s = gettimeofday(&now, NULL);
-    assert(s == 0);
+    /* struct timeval now; */
+    /* s = gettimeofday(&now, NULL); */
+    /* assert(s == 0); */
 
     if (found) {
-        printf("Found %d!\n", what);
+        printf("Found number\n");
     }
     else {
-        printf("Cound not find %d\n", what);
+        printf("Cound not find\n");
     }
 
-    int secs = now.tv_sec - then.tv_sec;
-    int msecs = now.tv_usec - then.tv_usec;
-    double elapsed = secs + (double)msecs / 1000000;
-    printf("Seconds elapsed: %lf\n", elapsed);
+    /* int secs = now.tv_sec - then.tv_sec; */
+    /* int msecs = now.tv_usec - then.tv_usec; */
+    /* double elapsed = secs + (double)msecs / 1000000; */
+    /* printf("Seconds elapsed: %lf\n", elapsed); */
 
     return 0;
 }
